27fd7b5a9860aaddfbc0a96bd7aba521e7d38f8a
rans0000 <rans0000@gmail.com>
2025-04-26 15:31:37 +0530

    create sketch to highlight objects using p5.js

--- src/scripts/camera-identify-p5.ts
+++ src/scripts/camera-identify-p5.ts
@@ -1,7 +1,15 @@
+import * as cocoSSD from '@tensorflow-models/coco-ssd';
+import * as tf from '@tensorflow/tfjs';
 import P5, { MediaElement } from 'p5';
 
 const topOffset = 63;
 let capture: MediaElement;
+let model: cocoSSD.ObjectDetection | null = null;
+const WIDTH = 400;
+const HEIGHT = 400;
+const CANVAS_OFFSET_X = 50;
+const CANVAS_OFFSET_Y = 50;
+let isProcessing = false;
 
 const sketch = (p5: P5) => {
     p5.setup = async () => {
@@ -11,17 +19,21 @@ const sketch = (p5: P5) => {
         p5.background('white');
         p5.pixelDensity(1);
         p5.colorMode(p5.RGB);
-        p5.noLoop();
         window.addEventListener('resize', () => resizeDisplay(p5));
 
         init(p5);
     };
 
-    p5.draw = () => {
-        capture.loadPixels();
-        console.log(capture.pixels.length);
+    p5.draw = async () => {
+        if (!model) return;
 
-        p5.image(capture, 0, 0, 200, 200);
+        capture.loadPixels();
+        p5.push();
+        p5.translate(CANVAS_OFFSET_X, CANVAS_OFFSET_Y);
+        p5.image(capture, 0, 0, WIDTH, HEIGHT);
+        p5.pop();
+        await predict(p5, capture.pixels);
+        isProcessing = false;
     };
 
     /**--------------------------------- */
@@ -35,13 +47,41 @@ const sketch = (p5: P5) => {
         const constraints = {
             video: {
                 noiseSuppression: true,
-                width: 200,
-                height: 200
+                width: WIDTH,
+                height: HEIGHT
             },
             audio: false
         };
-        capture = p5.createCapture(constraints, () => p5.loop()) as MediaElement;
+        capture = p5.createCapture(constraints, { flipped: true }, async () => {
+            model = await cocoSSD.load();
+        }) as MediaElement;
         capture.hide();
+    }
+
+    async function predict(p5: P5, pixels: number[] = []) {
+        if (!model) return;
+        isProcessing = true;
+        const total = WIDTH * HEIGHT;
+
+        const rgbaTensor = tf.tensor2d(pixels, [total, 4]);
+        const rgbTensor = rgbaTensor.slice([0, 0], [-1, 3]);
+        const rgbTensor3d = rgbTensor.reshape([WIDTH, HEIGHT, 3]) as tf.Tensor3D;
+
+        const predictions = await model.detect(rgbTensor3d);
+        p5.rectMode(p5.CORNER);
+        p5.stroke(255);
+        p5.noFill();
+        p5.textSize(20);
+        p5.push();
+        p5.translate(CANVAS_OFFSET_X, CANVAS_OFFSET_Y);
+        for (const item of predictions) {
+            if (item.score < 0.7) continue;
+            p5.rect(item.bbox[0], item.bbox[1], item.bbox[2], item.bbox[3]);
+            p5.text(item.class, item.bbox[0], item.bbox[1]);
+        }
+        p5.pop();
+
+        return;
     }
 };
 
