899d2f7f45d3bb0c8222a40d443e27eae511d5e5
rans0000 <rans0000@gmail.com>
2025-04-26 15:30:52 +0530

    manually overload 'createCapture' method to accept 'flipped' parameter which was missing from @types/p5 defenitions

--- src/types/p5-extend.d.ts
+++ src/types/p5-extend.d.ts
@@ -3,8 +3,14 @@ import * as p5 from 'p5';
 declare module 'p5' {
     interface p5InstanceExtensions {
         canvas: HTMLCanvasElement;
+        createCapture(type: string | TYPE | object, callback?: (...args: any[]) => any): Element;
+        createCapture(
+            type: string | TYPE | object,
+            obj?: { flipped: boolean },
+            callback?: (...args: any[]) => any
+        ): Element;
     }
-    export interface MediaElement {
+    interface MediaElement {
         loadPixels(): void;
         pixels: number[];
     }
