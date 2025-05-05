ef662cf00f50a7a711e9a3d659cdc70297f2a2a2
rans0000 <rans0000@gmail.com>
2025-05-05 13:19:03 +0530

    realestate prediction: load realestate data and normaize it

--- src/scripts/housing-prediction.ts
+++ src/scripts/housing-prediction.ts
@@ -1,3 +1,25 @@
 import { TRAINING_DATA } from '../models/real-estate-data.js';
+import * as tf from '@tensorflow/tfjs';
 
-console.log(TRAINING_DATA.inputs[0]);
+const inputTensor = tf.tensor2d(TRAINING_DATA.inputs);
+const outputTensor = tf.tensor1d(TRAINING_DATA.outputs);
+
+function normalize(tensor: tf.Tensor, _min?: number, _max?: number) {
+    const result = tf.tidy(() => {
+        const min = _min || tf.min(tensor, 0);
+        const max = _max || tf.max(tensor, 0);
+
+        const minSubtractedTensor = tf.sub(tensor, min);
+        const range = tf.sub(max, min);
+        const normalizedTensor = tf.div(minSubtractedTensor, range);
+
+        return { normalizedTensor, min, max };
+    });
+    return result;
+}
+
+const { normalizedTensor, min, max } = normalize(inputTensor);
+normalizedTensor.print();
+min instanceof tf.Tensor && min?.print();
+max instanceof tf.Tensor && max?.print();
+inputTensor.dispose();
