4b79d6c457ff32f90a4cff552e44fe3ff1335146
rans0000 <rans0000@gmail.com>
2025-05-06 19:51:53 +0530

    realestate prediction: display the table and train the model

--- src/scripts/housing-prediction.ts
+++ src/scripts/housing-prediction.ts
@@ -1,25 +1,90 @@
 import { TRAINING_DATA } from '../models/real-estate-data.js';
 import * as tf from '@tensorflow/tfjs';
 
-const inputTensor = tf.tensor2d(TRAINING_DATA.inputs);
-const outputTensor = tf.tensor1d(TRAINING_DATA.outputs);
+async function execute() {
+    const tableBody = document.getElementById('table-body') as HTMLBodyElement;
+    const inputTensor = tf.tensor2d(TRAINING_DATA.inputs);
+    const outputTensor = tf.tensor1d(TRAINING_DATA.outputs);
 
-function normalize(tensor: tf.Tensor, _min?: number, _max?: number) {
-    const result = tf.tidy(() => {
-        const min = _min || tf.min(tensor, 0);
-        const max = _max || tf.max(tensor, 0);
+    // render the table
+    buildTable(TRAINING_DATA.inputs, TRAINING_DATA.outputs, tableBody);
 
-        const minSubtractedTensor = tf.sub(tensor, min);
-        const range = tf.sub(max, min);
-        const normalizedTensor = tf.div(minSubtractedTensor, range);
+    // normalize data
 
-        return { normalizedTensor, min, max };
-    });
-    return result;
+    const { normalizedTensor, min, max } = normalize(inputTensor);
+    normalizedTensor.print();
+    min instanceof tf.Tensor && min?.print();
+    max instanceof tf.Tensor && max?.print();
+    inputTensor.dispose();
+
+    // training & prediction
+
+    const model = tf.sequential();
+    model.add(tf.layers.dense({ inputShape: [2], units: 1 }));
+    model.summary();
+
+    await train();
+
+    async function train() {
+        const learningRate = 0.001;
+
+        model.compile({
+            optimizer: tf.train.sgd(learningRate),
+            loss: 'meanSquaredError'
+        });
+
+        let result = await model.fit(normalizedTensor, outputTensor, {
+            validationSplit: 0.15,
+            shuffle: true,
+            batchSize: 64,
+            epochs: 10
+        });
+
+        outputTensor.dispose();
+        normalizedTensor.dispose();
+
+        console.log('Average error lose', Math.sqrt(result.history.loss[result.history.loss.length - 1] as number));
+        console.log(
+            'Average validation error lose',
+            Math.sqrt(result.history.val_loss[result.history.val_loss.length - 1] as number)
+        );
+    }
+
+    function normalize(tensor: tf.Tensor, _min?: number, _max?: number) {
+        const result = tf.tidy(() => {
+            const min = _min || tf.min(tensor, 0);
+            const max = _max || tf.max(tensor, 0);
+
+            const minSubtractedTensor = tf.sub(tensor, min);
+            const range = tf.sub(max, min);
+            const normalizedTensor = tf.div(minSubtractedTensor, range);
+
+            return { normalizedTensor, min, max };
+        });
+        return result;
+    }
+
+    function buildTable(input: number[][], output: number[], body: HTMLBodyElement) {
+        // const node = new
+        const fragment = document.createDocumentFragment();
+        for (let i = 0; i < input.length; i++) {
+            const row = document.createElement('tr');
+            const areaCell = document.createElement('td');
+            areaCell.textContent = `${input[i][0]}`;
+            areaCell.classList = 'p-2 text-right font-mono';
+            const roomCell = document.createElement('td');
+            roomCell.textContent = `${input[i][1]}`;
+            roomCell.classList = 'py-2 px-4 text-right font-mono';
+            const priceCell = document.createElement('td');
+            priceCell.textContent = `$${output[i].toFixed(2)}`;
+            priceCell.classList = 'p-2 text-right font-mono';
+            row.appendChild(areaCell);
+            row.appendChild(roomCell);
+            row.appendChild(priceCell);
+            fragment.appendChild(row);
+        }
+        body.appendChild(fragment);
+    }
 }
 
-const { normalizedTensor, min, max } = normalize(inputTensor);
-normalizedTensor.print();
-min instanceof tf.Tensor && min?.print();
-max instanceof tf.Tensor && max?.print();
-inputTensor.dispose();
+execute();
